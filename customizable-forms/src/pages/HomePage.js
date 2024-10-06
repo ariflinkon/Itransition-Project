5bc3506f6af0de020664f39f729616da151af64b
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-10-06 23:59:27 +0600

    Added Welcome homepage

---
+++ customizable-forms/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="home-page">
+      <h1>Welcome to Customizable Forms</h1>
+      <p>Create, fill, and analyze forms seamlessly.</p>
+    </div>
+  );
+};
+
+export default HomePage;
