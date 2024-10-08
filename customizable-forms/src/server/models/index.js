e46edb373c6573462dbcd893d5df4b01f09258fb
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-10-08 23:06:42 +0600

    Added index and user model in server side

---
+++ customizable-forms/src/server/models/index.js
@@ -0,0 +1,31 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const dbConfig = require('../config/db.config');
+
+const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: 'mysql', // Specify the dialect explicitly
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    }
+  });
+
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+// Test the connection to the database
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+// Models
+db.User = require('./user.model')(sequelize, DataTypes);
+
+module.exports = db;
---
+++ customizable-forms/src/server/models/user.model.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        role: {
+            type: DataTypes.STRING,
+            defaultValue: 'user' // can be 'user' or 'admin'
+        }
+    });
+    return User;
+};
