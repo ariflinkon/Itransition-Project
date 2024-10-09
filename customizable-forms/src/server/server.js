8d51d695276bf868555c052a75fb73039e60f40d
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-10-09 23:50:20 +0600

    Updated the serverjs

--- customizable-forms/src/server/server.js
+++ customizable-forms/src/server/server.js
@@ -4,16 +4,15 @@ const cors = require('cors');
 const db = require('./models/index');
 require('dotenv').config();
 
-
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
 // Sync DB
-db.sequelize.sync();
+db.sequelize.sync({ alter: true })  // Use alter in non-prod environments to avoid data loss
 
 // Starting server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
