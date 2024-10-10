468ca7a360136fa4bee44f0ec25c91c26a3c9d91
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-10-10 23:59:31 +0600

    Updated server file

--- customizable-forms/src/server/server.js
+++ customizable-forms/src/server/server.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const db = require('./models/index');
+const db = require('./models');
+const userRoutes = require('./routes/auth.routes');
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-
+app.use('/', userRoutes);
 // Sync DB
-db.sequelize.sync({ alter: true })  // Use alter in non-prod environments to avoid data loss
-
+db.sequelize.sync({ alter: true }) 
 // Starting server
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
