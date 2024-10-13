09faff0e68ff2d2ca35791fbb847d20da7f62734
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-10-13 23:59:06 +0600

    Updated index file

--- customizable-forms/src/index.js
+++ customizable-forms/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './client/styles/index.css';
-import './client/styles/main.css'; 
+import './client/styles/main.scss';
 import App from './client/App';
-import reportWebVitals from './client/reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-reportWebVitals();
