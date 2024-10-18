3c5d750fd704e720fe868e1eeb6ce6a18ae23951
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-10-18 23:43:33 +0600

    Updated user.controller file

--- customizable-forms/src/server/controllers/user.controller.js
+++ customizable-forms/src/server/controllers/user.controller.js
@@ -1,36 +1,64 @@
-const db = require('../models');
-const User = db.User;
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth.config');
+const db = require("../models");
+const User = db.user;
 
-exports.register = async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const newUser = await User.create({ username, email, password: hashedPassword });
-        res.status(201).send(newUser);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: 'Error registering user.' });
-    }
+// Get all users (Admin Only)
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).send(users);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
 
-exports.login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await User.findOne({ where: { email } });
-        if (!user) {
-            return res.status(404).send({ message: 'User not found' });
-        }
-        const passwordIsValid = await bcrypt.compare(password, user.password);
-        if (!passwordIsValid) {
-            return res.status(401).send({ message: 'Invalid Password' });
-        }
-        const token = jwt.sign({ id: user.id }, authConfig.secret, { expiresIn: 86400 });
-        res.status(200).send({ accessToken: token });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send({ message: 'Error logging in.' });
+// Block a user
+exports.blockUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
     }
+
+    user.isBlocked = true;
+    await user.save();
+
+    res.status(200).send({ message: "User blocked successfully" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+// Unblock a user
+exports.unblockUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    user.isBlocked = false;
+    await user.save();
+
+    res.status(200).send({ message: "User unblocked successfully" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+};
+
+// Delete a user
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id);
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    await user.destroy();
+    res.status(200).send({ message: "User deleted successfully" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 };
