c66fcb556f0b3f8d25f7ab9c418a20df7857984c
Arif Jahan Linkon <arifjahanlinkon7931@gmail.com>
2024-11-07 22:29:13 +0600

    Updated server file

--- customizable-forms/src/server/server.js
+++ customizable-forms/src/server/server.js
@@ -1,27 +1,26 @@
+// src/server.js
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const db = require("./models");
 const routes = require("./routes");
+const salesforceRoutes = require('./routes/salesforceRoutes');
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Routes
 app.use("/api", routes);
+app.use("/api/salesforce", salesforceRoutes);
 
-// Database connection
 db.sequelize.sync().then(() => {
   console.log("Database connected and synced.");
 }).catch((err) => {
   console.error("Failed to sync database: ", err);
 });
 
-// Set the port and start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
